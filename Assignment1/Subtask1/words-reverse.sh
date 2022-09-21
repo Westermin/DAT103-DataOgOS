caa51fb7946c2a7cb06c7c46657456724056b901
westermin <reaverwester@gmail.com>
2022-09-21 13:42:02 +0300

    Changes by Torva

--- Assignment1/Subtask1/words-reverse.sh
+++ Assignment1/Subtask1/words-reverse.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
-read s
-strlen=${#s}
-for (( i=$strlen-1; i>=0; i-- ));
-do
-    revstr=$revstr${s:$i:1}
-done
-echo "Original String : $s"
-echo "Reversed String : $revstr"
+mktemp -d
 
-read test
+read depunctuation.sh
+
+
+#read s
+#strlen=${#s}
+#for (( i=$strlen-1; i>=0; i-- ));
+#do
+#    revstr=$revstr${s:$i:1}
+#done
+#echo "Original String : $s"
+#echo "Reversed String : $revstr"
+
+#read test
+
+function cleanup {
+  rm -rf "$WORK_DIR"
+  echo "Deleted temp working directory $WORK_DIR"
+}
+
