456d6c6a651b41474e4b583fd66c9d0fd21d6111
westermin <reaverwester@gmail.com>
2022-09-22 18:27:06 +0200

    Fikset depunctuate.sh og Repunctuation.sh, la til et cache for å lagre indeks til lagring av komma og punktum sine posisjoner.

--- Assignment1/Subtask2/depunctuate.sh
+++ Assignment1/Subtask2/depunctuate.sh
@@ -1,37 +1,29 @@
 #! /usr/bin/bash
-#echo "Enter some text "
-
-# Get a value:
-
 file=hello.txt
 
 getPos(){
-  declare -a a1
-  declare -a a2
-  declare -i pos=0
-while read -n1 c; do
-    #echo "$c"
-  if  [ "$c" = "," ]
-  then
-    ##hashmap
-      c=" "
-      echo "fant en: $pos"
-      a1+=($pos)
-      a2+=(",")
+declare -a a1
+declare -a a2
+declare -i pos=0
+  while read -n1 c; do
+      #echo "$c"
+    if  [ "$c" = "," ]
+      then
+          c=" "
+          echo "fant en: $pos"
+          a1+=($pos)
+          a2+=(",")
 
-  elif [ "$c" = '.' ]
-  then
-    echo "fant en: $pos"
-    a1+=($pos)
-    a2+=(".")
-  fi
-  pos=pos+1
+      elif [ "$c" = '.' ]
+      then
+        echo "fant en: $pos"
+        a1+=($pos)
+        a2+=(".")
+    fi
+    pos=pos+1
+  done < $file
 
-  echo "$pos"
-done < $file
-{ echo "${a1[*]}"; echo "${a2[*]}";} >cache.txt
-
-
+  { echo "${a1[*]}"; echo "${a2[*]}";} >cache.txt
 }
 
 
@@ -39,7 +31,6 @@ dePunctuate() {
 cat hello.txt | tr -d '[:punct:]' > hello.txt
 }
 
-
+read input
 getPos "$input"
 dePunctuate "$input"
-read input
